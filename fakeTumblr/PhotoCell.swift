3874d5e024a01e6bfded94cedb366c332ed1de11
Emma Malysz <emalysz@usc.edu>
2016-09-20 19:12:51 -0700

    first commit

---
+++ fakeTumblr/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  fakeTumblr
+//
+//  Created by Chaitanya Pilaka on 9/19/16.
+//  Copyright © 2016 Chaitanya Pilaka. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ fakeTumblr/PhotoDetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PhotoDetailsViewController.swift
+//  fakeTumblr
+//
+//  Created by Chaitanya Pilaka on 9/19/16.
+//  Copyright © 2016 Chaitanya Pilaka. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
